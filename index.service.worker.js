5da9fa63832ab6843a0b506a53c0e5d0d74bd3ba
Jinyoung-King <Jinyoung-King@users.noreply.github.com>
2026-06-29 16:53:31 +0000

    deploy: fe0a7b0812cbe8d5162bc5731d6ad1973f75cff8

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1782750902|2843940';
+const CACHE_VERSION = '1782752008|2741538';
 /** @type {string} */
 const CACHE_PREFIX = 'soulbinder-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
